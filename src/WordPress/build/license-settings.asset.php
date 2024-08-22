1cdfda57f1a57057ff92bd4bc1a3994b705c47f2
Clayton Rogers <mr.clayton@paymentplugins.com>
2024-08-22 14:32:55 -0700

    update

--- src/WordPress/build/license-settings.asset.php
+++ src/WordPress/build/license-settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'paidcommunities-wordpress-api', 'wp-polyfill'), 'version' => '04b00ce370ecf6632d9a');
+<?php return array('dependencies' => array('jquery', 'paidcommunities-wordpress-api', 'wp-polyfill'), 'version' => '5bacf5d2b4fb90837a7b');
--- src/WordPress/build/react-components.asset.php
+++ src/WordPress/build/react-components.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('paidcommunities-wordpress-api', 'wp-components', 'wp-element', 'wp-polyfill'), 'version' => '22563c7bde2d5ee1cb03');
+<?php return array('dependencies' => array('paidcommunities-wordpress-api', 'wp-components', 'wp-element', 'wp-polyfill'), 'version' => '5f061779242ea913e15b');
