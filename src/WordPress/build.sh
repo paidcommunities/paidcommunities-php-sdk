3d02933c353cb437619e265bb8ca3b5f7d4b5995
Clayton Rogers <mr.clayton@paymentplugins.com>
2024-10-25 16:52:46 -0700

    update

--- src/WordPress/build.sh
+++ src/WordPress/build.sh
@@ -1,22 +1,26 @@
 BASE_PATH=$(pwd)
 
+rm -rf "${BASE_PATH}/build"
+
+mkdir -p "${BASE_PATH}/build"
+
 # clone repository to this directory. We use github2.com so the paidcommunities ssh key is used
 echo 'cloning paidcommunities-wp repository...'
 git clone git@github2.com:paidcommunities/paidcommunities-wp.git
 
 cd paidcommunities-wp
 
-echo 'npm install...'
-
-npm install
+if [ -d "node_modules" ]; then
+  echo "Directory exists. Running npm update..."
+  npm update
+else
+  echo "Directory does not exist. Running npm install..."
+  npm install
+fi
 
 npm run build:prod
-
-rm -rf "${BASE_PATH}/build"
 
 echo 'copying dist directory to build'
 cp -R dist/* "${BASE_PATH}/build"
 
-cd BASE_PATH
-
-rm -rf paidcommunities-wp
+cd "$BASE_PATH" || exit
