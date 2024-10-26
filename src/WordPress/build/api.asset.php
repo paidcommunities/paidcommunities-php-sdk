aeffbccf3898d0c60dca93bbe119844b33dc9fdb
Clayton Rogers <mr.clayton@paymentplugins.com>
2024-10-26 14:31:54 -0700

    update

--- src/WordPress/build/api.asset.php
+++ src/WordPress/build/api.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-polyfill'), 'version' => '890931f5b14665a206a8');
+<?php return array('dependencies' => array('jquery', 'wp-polyfill'), 'version' => '6505bc36cd9c3b875c29');
