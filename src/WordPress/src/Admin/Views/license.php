11c4080a84d2a5fc661c74b9cb8b616430d5aec0
Clayton Rogers <mr.clayton@paymentplugins.com>
2024-10-26 14:42:11 -0700

    update

--- src/WordPress/src/Admin/Views/license.php
+++ src/WordPress/src/Admin/Views/license.php
@@ -7,7 +7,7 @@
 					<?php if ( $license->isRegistered() ): ?>
                         <input id="<?php echo esc_attr( $name ) ?>-license_key" class="PaidCommunitiesInput-text LicenseKey" type="text" disabled value="<?php echo esc_attr( $license->getLicenseKey() ) ?>"/>
 					<?php else: ?>
-                        <input id="<?php echo esc_attr( $name ) ?>-license_key" class="PaidCommunitiesInput-text LicenseKey"/>
+                        <input id="<?php echo esc_attr( $name ) ?>-license_key" class="PaidCommunitiesInput-text LicenseKey" type="text"/>
 					<?php endif ?>
                 </div>
 				<?php if ( $license->isRegistered() ): ?>
