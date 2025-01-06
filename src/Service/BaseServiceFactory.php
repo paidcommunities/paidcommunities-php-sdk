24d34a71427a1364ca7ad63129d8dcd5ccb89143
Clayton Rogers <mr.clayton@paymentplugins.com>
2025-01-05 19:09:51 -0800

    update

--- src/Service/BaseServiceFactory.php
+++ src/Service/BaseServiceFactory.php
@@ -7,7 +7,8 @@ class BaseServiceFactory extends AbstractServiceFactory {
 	protected $mappings = [
 		'updates'            => UpdateService::class,
 		'domains'            => DomainService::class,
-		'domainRegistration' => DomainRegistrationService::class
+		'domainRegistration' => DomainRegistrationService::class,
+		'productInfo'        => ProductInfoService::class
 	];
 
 	public function __get( $name ) {
